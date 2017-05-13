c54245a4068e7c017343f856ccbe8761103eab29
lpy <alpay.ozkeskin@bilgiedu.net>
2017-05-13 21:58:51 +0300

    started newton polynomial

---
+++ newton_form.py
@@ -0,0 +1,16 @@
+import numpy
+
+def newton(x, y, degree):
+    if degree > length(x) - 1:
+        return none
+    a = []
+    f = []
+    a[0] = 1
+    f[0] = 1;
+    for i in range(1, n + 1):
+        
+
+
+
+
+def eval_newton(f, var):
